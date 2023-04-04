c5a3e46795faf186b15511cdd6ccbf723fa618f5
jwkidd3 <jkidd@kiddcorp.com>
2023-04-04 12:18:45 -0500

    updates

--- labs/03-scheduler/scripts/scheduler.sh
+++ labs/03-scheduler/scripts/scheduler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl proxy --port=9090 &
+kubectl proxy --port=9092 &
 while true;
 do
     for PODNAME in $(kubectl get pods -o json | jq '.items[] | select(.spec.schedulerName == "my-scheduler") | select(.spec.nodeName == null) | .metadata.name' | tr -d '"');
@@ -8,7 +8,7 @@ do
         NUMNODES=${#NODES[@]}
         CHOSEN=${NODES[$[ $RANDOM % $NUMNODES ]]}
         curl --header "Content-Type:application/json" --request POST --data '{"apiVersion":"v1", "kind": "Binding", "metadata": {"name": "'$PODNAME'"}, "target": {"apiVersion": "v1", "kind"
-: "Node", "name": "'$CHOSEN'"}}' http://localhost:9090/api/v1/namespaces/default/pods/$PODNAME/binding/
+: "Node", "name": "'$CHOSEN'"}}' http://localhost:9092/api/v1/namespaces/default/pods/$PODNAME/binding/
         echo "Assigned $PODNAME to $CHOSEN"
     done
     sleep 1
